6cec81d2d55b7769a83e91399d33fc563de265d3
david jensen <djensen@lyonscg.com>
2016-05-11 13:57:21 -0400

    cleaned up the API calls, and commented the code

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,40 @@ function ISSFlyover(){
   this.refreshData();
   _.bindAll(this, 'refreshData', 'gotRefreshedData');
   $('#refresh').on('click', this.refreshData);
-  // $('#latitude, #longitude').on('change keypress', _.debounce(this.refreshData, 750));
+  $('#latitude, #longitude').on('change keypress', _.debounce(this.refreshData, 750));
 }
 _.extend(ISSFlyover.prototype, {
-  gotRefreshedData: function(iss, weather, astros){
-    var flyovers = _.map(iss.response, processFlyoverData);
+  gotRefreshedData: function(){
+    var me = this;
+    //gets all the people currently on the ISS, and prints out their names
+    var atTheISS = _.pluck(_.where(this.astros.people, {craft: "ISS"}), "name");
+    $('.astronauts').text(atTheISS.join(', '));
+
+    //prints out the rise time, and the weather
+    function outputFlyover(flyover, i){
+      $('.flyovers').append('<div>Flyover at ' + flyover.risetime + ':' + flyover.weatherDescription +'</div');
+    }
+
+    function processFlyoverData(flyover){
+      var weatherAtFlyover = _.find(me.weather.list, function(w){
+        return w.dt <= flyover.risetime && w.dt + 60*60*3 > flyover.risetime;
+      });
+
+      return{
+        clouds: weatherAtFlyover && weatherAtFlyover.clouds.all,
+        hasWeather: !_.isUndefined(weatherAtFlyover),
+        weatherDescription: weatherAtFlyover && weatherAtFlyover.weather[0].description,
+        risetime: new Date(flyover.risetime * 1000),
+        duration: flyover.duration
+      };
+    }
+
+    var flyovers = _.map(this.iss.response, processFlyoverData);
     var flyoversWithWeather = _.where(flyovers, {hasWeather: true});
     var flyoversGrouped = _.groupBy(flyoversWithWeather, getDay);
     var days = _.keys(flyoversGrouped);
     var summary = _.countBy(flyoversWithWeather, 'weatherDescription');
-    var atTheISS = _.pluck(_.where(astros.people, {craft: "ISS"}), "name");
+    var atTheISS = _.pluck(_.where(this.astros.people, {craft: "ISS"}), "name");
 
     $('.astronauts').text(atTheISS.join(', '));
 
@@ -23,24 +47,6 @@ _.extend(ISSFlyover.prototype, {
     function getDay(flyover){
       return flyover.risetime.toDateString();
     }
-
-    function outputFlyover(flyover, i){
-      $('.flyovers').append('<div>Flyover at ' + flyover.risetime + ':' + flyover.weatherDescription +'</div');
-    }
-
-    function processFlyoverData(flyover){
-      var weatherAtFlyover = _.find(weather.list, function(w){
-        return w.dt <= flyover.risetime && w.dt + 60*60*3 > flyover.risetime;
-      });
-
-      return{
-        clouds: weatherAtFlyover && weatherAtFlyover.clouds.all,
-        hasWeather: !_.isUndefined(weatherAtFlyover),
-        weatherDescription: weatherAtFlyover && weatherAtFlyover.weather[0].description,
-        risetime: new Date(flyover.risetime * 1000),
-        duration: flyover.duration
-      };
-  }
 
     _.each(days, function(day){
       $('.flyovers').append('<h2>'+ day +'</h2>');
@@ -54,13 +60,18 @@ _.extend(ISSFlyover.prototype, {
     var location = {lat: $('#latitude').val(), lon: $('#longitude').val()};
     var me = this;
     var apiKey = "ef3d980bb7bf92cc26ddafb1e5e10b1c";
-    jQuery.getJSON("http://api.open-notify.org/iss-pass.json?lat=50.8&lon=-0.3667&n=20&callback=?", _.extend({n: 100}, location), function (iss) {
-      jQuery.getJSON("http://api.openweathermap.org/data/2.5/forecast?&APPID="+ apiKey +"&callback=?", location, function (weather) {
-        jQuery.getJSON("http://api.open-notify.org/astros.json?callback=?", function (astros) {
-          me.gotRefreshedData(iss, weather, astros);
-        });
-      });
-    });
+
+    var complete = _.after(3, this.gotRefreshedData);
+
+    var getData = function(property, url, options){
+      jQuery.getJSON(url, options, function(data){
+        me[property] = data;
+        complete();
+      })
+    };
+    getData('iss', "http://api.open-notify.org/iss-pass.json?lat=50.8&lon=-0.3667&n=20&callback=?", _.extend({n: 100}, location));
+    getData('weather', "http://api.openweathermap.org/data/2.5/forecast?&APPID="+ apiKey +"&callback=?", location);
+    getData('astros', "http://api.open-notify.org/astros.json?callback=?", {})
   }
 });
 
